c087a164fdaee71140aa83e7a3108750d8d6a36f
Robert Lim <robert.lim@lexicondigital.com.au>
2022-12-10 16:49:52 +1100

    Add a day 10 answer and fix all the module BS

---
+++ Day10/src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    var signal = 1
+    var cycle = 0
+    val cycles = arrayOf(19, 59, 99, 139, 179, 219)
+    var totalSignalStrength = 0
+    File(args[0]).forEachLine { line ->
+        var num = 0
+        var doCycles = 0
+        if (line == "noop") {
+            // do nothing
+            doCycles = 1
+        } else if (line.startsWith("addx")) {
+            num = line.substring(5, line.length).toInt()
+            doCycles = 2
+        }
+        repeat((1..doCycles).count()) {
+            if (cycles.contains(cycle)) {
+                totalSignalStrength += (cycle + 1) * signal
+            }
+            val linePos = cycle % 40
+            val minSprite = (signal % 40) - 1
+            val maxSprite = (signal % 40) + 1
+            print(if (linePos in minSprite..maxSprite) "#" else ".")
+            cycle++
+            if (cycle % 40 == 0) print("\n")
+        }
+        signal += num
+    }
+
+    println(totalSignalStrength)
+}
--- Day8/src/main/kotlin/Main.kt
+++ Day8/src/main/kotlin/Main.kt
@@ -2,40 +2,39 @@ import java.io.File
 import kotlin.math.max
 
 fun main(args: Array<String>) {
-    val lines = mutableListOf<List<Int>>()
-    File(args[0]).forEachLine { line ->
-        lines.add(line.map { c -> c.code - '0'.code })
-    }
+    val lines = File(args[0]).readLines().map(String::toCharArray).map{ it.map(Char::digitToInt)}
     var maxScore = 0
-    for((row, trees) in lines.withIndex()) {
-        for((col, tree) in trees.withIndex()) {
+    for ((row, trees) in lines.withIndex()) {
+        for ((col, tree) in trees.withIndex()) {
             var score = 0
             var cumulative = 1
             // Look left
-            trees.subList(0, col).reversed().firstOrNull{
+            trees.subList(0, col).reversed().firstOrNull {
                 score++
                 it >= tree
             }
             cumulative *= score
-            score = 0
+
             println("Score for left [$row][$col]=$score")
             // Look Right
-            trees.subList(col+1, trees.size).firstOrNull {
+            score = 0
+            trees.subList(col + 1, trees.size).firstOrNull {
                 score++
                 it >= tree
             }
             cumulative *= score
-            score = 0
+
             println("Score for right [$row][$col]=$score")
+            score = 0
             // Look up
             lines.subList(0, row).reversed().firstOrNull {
                 score++
                 it[col] >= tree
             }
             cumulative *= score
-            score = 0
             println("Score for up [$row][$col]=$score")
-            lines.subList(row+1, lines.size).firstOrNull {
+            score = 0
+            lines.subList(row + 1, lines.size).firstOrNull {
                 score++
                 it[col] >= tree
             }
