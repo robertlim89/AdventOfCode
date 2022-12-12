a0269bfcf315b00c30a92ad23ff01f82d325db45
Robert Lim <robert.lim@lexicondigital.com.au>
2022-12-12 16:16:11 +1100

    Start day 12

--- Day12/src/main/kotlin/Main.kt
+++ Day12/src/main/kotlin/Main.kt
@@ -1,3 +1,22 @@
+import java.io.File
+
 fun main(args: Array<String>) {
-    
+    val map = mutableListOf<List<Pair<Int, Type>>>()
+    File(args[0]).forEachLine { line ->
+        val heights = line.toCharArray().map { it.toHeight() }
+        println(heights)
+        map.add(heights)
+    }
+}
+
+fun Char.toHeight() : Pair<Int, Type> {
+    if(this == 'S') return Pair(1, Type.START)
+    if(this == 'E') return Pair(26, Type.END)
+    return "_abcdefghijklmnopqrstuvwxyz".indexOfFirst { it == this } to Type.PATH
+}
+
+enum class Type {
+    START,
+    END,
+    PATH
 }
